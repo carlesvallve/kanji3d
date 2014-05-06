c233994d53adf5f2c5e1effdbb5cbb0ad0ea9095
Carles Vallve <snaptothegrid@gmail.com>
2014-05-06 21:11:56 +0900

    cleanup

--- scripts/battle.js
+++ scripts/battle.js
@@ -6,11 +6,11 @@ var Battle = function (container) {
     //self.bg.style.opacity = 0.1;
 
     this.init = function () {
-        //self.elm.style.opacity = 0;
-        /*this.loadBackground('assets/textures/gif/' + utils.randomInt(1,5) + '.gif', function (img) {
+        self.elm.style.opacity = 0;
+        this.loadBackground('assets/textures/gif/' + utils.randomInt(1,5) + '.gif', function (img) {
             self.bg.style.background = 'url(' + img.src + ') no-repeat ' + utils.randomInt(0, 100) + '% 95%';
             tweener.tween(self.elm, { opacity: 1 }, { time: 500, delay: 500, easing: window.easing }, function () {});
-        });*/
+        });
     };
 
 
